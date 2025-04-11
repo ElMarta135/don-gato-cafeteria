4f35bc9e48523af6fb2626f95cc57ddac788da15
ElMarta135 <martiin.rtl@gmail.com>
2025-04-11 03:40:11 -0400

    menu

--- app.js
+++ app.js
@@ -2,44 +2,61 @@ const productos = {
   cafes: [
     {
       nombre: "Espresso",
-      precio: 2.000,
+      precio: 2000,
       descripcion: "Café intenso",
-      imagen: "https://www.cuisinart.com/dw/image/v2/ABAF_PRD/on/demandware.static/-/Sites-us-cuisinart-sfra-Library/default/dw2ca0aa66/images/recipe-Images/cafe-latte1-recipe.jpg?sw=1200&sh=1200&sm=fit",
+      imagen: "https://www.cuisinart.com/dw/image/v2/ABAF_PRD/on/demandware.static/-/Sites-us-cuisinart-sfra-Library/default/dw2ca0aa66/https://blog.scoolinary.com/la-historia-del-cafe-expresso-un-viaje-por-el-mundo/recipe-Images/cafe-latte1-recipe.jpg?sw=1200&sh=1200&sm=fit",
     },
     {
       nombre: "Latte",
-      precio: 3.000,
+      precio: 3000,
       descripcion: "Con leche espumada",
       imagen: "https://www.cuisinart.com/recipes/beverages/cafe-latte-recipe.html",
     },
     {
       nombre: "Americano",
-      precio: 3.000,
+      precio: 2500,
       descripcion: "Shot de espresso con aguaa",
-      imagen: "https://www.cuisinart.com/recipes/beverages/cafe-latte-recipe.html",
+      imagen: "https://elsumario.com/cual-es-el-origen-del-cafe-americano/",
     },
     {
       nombre: "Café helado",
-      precio: 3.000,
-      descripcion: "Café con scoop de helado",
-      imagen: "https://www.cuisinart.com/recipes/beverages/cafe-latte-recipe.html",
-      },
-      
+      precio: 5000,
+      descripcion: "Café con helado y galleta",
+      imagen: "https://www.biotona.be/es/blog/recetas/cafe-helado-proteico-con-canamo-en-polvo/",
+    },      
   ],
   postres: [
     {
       nombre: "Cheesecake",
-      precio: 3.500,
+      precio: 3000,
       descripcion: "Tarta de queso",
-      imagen: "https://via.placeholder.com/100",
+      imagen: "https://www.abc.es/recetasderechupete/pastel-de-queso-o-cheesecake-malamadre/21157/",
+    },
+    {
+      nombre: "Torta 3 leches",
+      precio: 3500,
+      descripcion: "Trozo torta 3 leches",
+      imagen: "https://www.recetasgratis.net/receta-de-torta-tres-leches-8910.html",
     },
   ],
   bebidas: [
     {
       nombre: "Té helado",
-      precio: 2.500,
+      precio: 2000,
       descripcion: "Refrescante",
-      imagen: "https://via.placeholder.com/100",
+      imagen: "https://www.mundodeportivo.com/uncomo/comida/receta/como-preparar-te-helado-15385.html",
+    },
+    {
+      nombre: "Jugos naturales",
+      precio: 3500,
+      descripcion: "Jugo natural sabor: Naranja, piña, maracuyá",
+      imagen: "https://www.alimentossas.com/blog/bebidas/jugos-naturales",
+    },
+    {
+      nombre: "Limonada",
+      precio: 3500,
+      descripcion: "Agua saborizada con frutas",
+      imagen: "https://www.cocinatis.com/receta/limonada.html",
     },
   ],
 };
